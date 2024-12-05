3570774e3fcc2b26d53ef1e355192b46956ba2c5
Quentinmac mini <bm.quentin@icloud.com>
2024-12-05 21:22:01 +0100

    please

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker run -p 9998:9998 --name kms ghcr.io/cosmian/kms:4.20.0
+docker run -d -it -p 9998:9998 -p 2222:22 --name data secure-data
+
+USER="bob"
+HOST="10.10.1.21"
+PORT=2222
+PASSWORD="root123"
+sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no -p "$PORT" "$USER@$HOST" "/usr/local/bin/first-load.sh"
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Exécution de first load"
-exec /usr/local/bin/first-load.sh
-
 echo "Exécution de SSH"
 exec /usr/sbin/sshd -D
 
