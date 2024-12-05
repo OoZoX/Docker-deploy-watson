8254172f9f7684444b68eb12a8d56957c1ba5ece
Quentinmac mini <bm.quentin@icloud.com>
2024-12-05 22:33:11 +0100

    a

--- first-load.sh
+++ first-load.sh
@@ -14,8 +14,8 @@ echo "$PASSWORD" | sudo -S mkdir -p $BOB_DIR $PUBLIC_DIR $ALICE_DIR $KEY_DIR
 # Générer une paire de clés RSA pour le dossier public
 RSA_PRIVATE_KEY="$KEY_DIR/public_rsa_private.key"
 RSA_PUBLIC_KEY="$KEY_DIR/public_rsa_public.key"
-openssl genrsa -out $RSA_PRIVATE_KEY 2048
-openssl rsa -in $RSA_PRIVATE_KEY -pubout -out $RSA_PUBLIC_KEY
+echo "$PASSWORD" | sudo -S openssl genrsa -out $RSA_PRIVATE_KEY 2048
+echo "$PASSWORD" | sudo -S openssl rsa -in $RSA_PRIVATE_KEY -pubout -out $RSA_PUBLIC_KEY
 echo "Clés RSA générées pour le dossier public : $RSA_PRIVATE_KEY et $RSA_PUBLIC_KEY"
 
 # Générer des clés symétriques avec CKMS
