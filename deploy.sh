f542bdb4f2b420f204551e2720c551ea152fc23b
Quentinmac mini <bm.quentin@icloud.com>
2024-12-05 22:30:34 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 docker run -d -it -p 9998:9998 -p 2222:22 --name data secure-data
+docker exec -itd data /usr/sbin/sshd -D
 
 USER="bob"
 HOST="10.10.1.21"
 PORT=2222
 PASSWORD="root123"
+
 ssh-keygen -R 10.10.1.21
 ssh-keyscan -p 2222 10.10.1.21 >> /home/docker-ecole/.ssh/known_hosts
 sshpass -p "$PASSWORD" ssh -p "$PORT" "$USER@$HOST" "/usr/local/bin/first-load.sh"
