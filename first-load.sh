fe52536e9918da2015da5669af35d6f886b5a8b8
Quentinmac mini <bm.quentin@icloud.com>
2024-12-05 21:09:17 +0100

    aaa

--- first-load.sh
+++ first-load.sh
@@ -5,6 +5,7 @@
 BOB_DIR="/secure/bob"
 ALICE_DIR="/secure/alice"
 PUBLIC_DIR="/secure/public"
+KEY_DIR="/secure/keys"
 
 # Création des répertoires
 mkdir -p $BOB_DIR $PUBLIC_DIR $ALICE_DIR
@@ -17,8 +18,8 @@ openssl rsa -in $RSA_PRIVATE_KEY -pubout -out $RSA_PUBLIC_KEY
 echo "Clés RSA générées pour le dossier public : $RSA_PRIVATE_KEY et $RSA_PUBLIC_KEY"
 
 # Générer des clés symétriques avec CKMS
-ckms sym keys create --number-of-bits 256 --algorithm aes --tag key-bob-aes || { echo "Erreur CKMS : création de key-bob-aes échouée"; exit 1; }
-ckms sym keys create --number-of-bits 256 --algorithm aes --tag key-alice-aes || { echo "Erreur CKMS : création de key-alice-aes échouée"; exit 1; }
+ckms -- sym keys create --number-of-bits 256 --algorithm aes --tag key-bob-aes || { echo "Erreur CKMS : création de key-bob-aes échouée"; exit 1; }
+ckms -- sym keys create --number-of-bits 256 --algorithm aes --tag key-alice-aes || { echo "Erreur CKMS : création de key-alice-aes échouée"; exit 1; }
 echo "Clés CKMS créées pour Bob et Alice."
 
 exec bash
