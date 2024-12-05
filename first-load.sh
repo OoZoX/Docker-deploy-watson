70b4d8fbcb234ca58f7030e8caf38b3d6c9b90bc
Quentinmac mini <bm.quentin@icloud.com>
2024-12-05 21:12:25 +0100

    a

--- first-load.sh
+++ first-load.sh
@@ -18,8 +18,8 @@ openssl rsa -in $RSA_PRIVATE_KEY -pubout -out $RSA_PUBLIC_KEY
 echo "Clés RSA générées pour le dossier public : $RSA_PRIVATE_KEY et $RSA_PUBLIC_KEY"
 
 # Générer des clés symétriques avec CKMS
-ckms -- sym keys create --number-of-bits 256 --algorithm aes --tag key-bob-aes || { echo "Erreur CKMS : création de key-bob-aes échouée"; exit 1; }
-ckms -- sym keys create --number-of-bits 256 --algorithm aes --tag key-alice-aes || { echo "Erreur CKMS : création de key-alice-aes échouée"; exit 1; }
+ckms sym keys create --number-of-bits 256 --algorithm aes --tag key-bob-aes || { echo "Erreur CKMS : création de key-bob-aes échouée"; exit 1; }
+ckms sym keys create --number-of-bits 256 --algorithm aes --tag key-alice-aes || { echo "Erreur CKMS : création de key-alice-aes échouée"; exit 1; }
 echo "Clés CKMS créées pour Bob et Alice."
 
 exec bash
