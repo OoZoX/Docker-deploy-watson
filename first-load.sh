37869e1ff209412a21dda7c39ac1ba2aada47adf
Quentinmac mini <bm.quentin@icloud.com>
2024-12-05 21:56:56 +0100

    add sudo

--- first-load.sh
+++ first-load.sh
@@ -7,14 +7,15 @@ ALICE_DIR="/secure/alice"
 PUBLIC_DIR="/secure/public"
 KEY_DIR="/secure/keys"
 
+PASSWORD="root123"
 # Création des répertoires
-mkdir -p $BOB_DIR $PUBLIC_DIR $ALICE_DIR $KEY_DIR
+echo "$PASSWORD" | sudo mkdir -p $BOB_DIR $PUBLIC_DIR $ALICE_DIR $KEY_DIR
 
 # Générer une paire de clés RSA pour le dossier public
 RSA_PRIVATE_KEY="$KEY_DIR/public_rsa_private.key"
 RSA_PUBLIC_KEY="$KEY_DIR/public_rsa_public.key"
-openssl genrsa -out $RSA_PRIVATE_KEY 2048
-openssl rsa -in $RSA_PRIVATE_KEY -pubout -out $RSA_PUBLIC_KEY
+echo "$PASSWORD" | sudoopenssl genrsa -out $RSA_PRIVATE_KEY 2048
+echo "$PASSWORD" | sudoopenssl rsa -in $RSA_PRIVATE_KEY -pubout -out $RSA_PUBLIC_KEY
 echo "Clés RSA générées pour le dossier public : $RSA_PRIVATE_KEY et $RSA_PUBLIC_KEY"
 
 # Générer des clés symétriques avec CKMS
