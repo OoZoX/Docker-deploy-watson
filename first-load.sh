342c156f8f393ef8b3c0f032334284bdd149effb
Quentinmac mini <bm.quentin@icloud.com>
2024-12-05 22:49:52 +0100

    perm

--- first-load.sh
+++ first-load.sh
@@ -11,6 +11,16 @@ PASSWORD="root123"
 # Création des répertoires
 echo "$PASSWORD" | sudo -S mkdir -p $BOB_DIR $PUBLIC_DIR $ALICE_DIR $KEY_DIR
 
+# Changement propriétaire repertoir
+echo "$PASSWORD" | sudo -S chown alice /secure/alice
+echo "$PASSWORD" | sudo -S chown bob /secure/bob
+
+echo "$PASSWORD" | sudo -S groupadd public
+echo "$PASSWORD" | sudo -S usermod -a -G public alice
+echo "$PASSWORD" | sudo -S usermod -a -G public bob
+echo "$PASSWORD" | sudo -S chgrp public /secure/public
+echo "$PASSWORD" | sudo -S chmod 777 public
+
 # Générer une paire de clés RSA pour le dossier public
 RSA_PRIVATE_KEY="$KEY_DIR/public_rsa_private.key"
 RSA_PUBLIC_KEY="$KEY_DIR/public_rsa_public.key"
